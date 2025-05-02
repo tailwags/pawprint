43180f609fdb5b125e1fbb75479604c7164a8cd4
Matilde Morrone <contact@morrone.dev>
2025-05-02 10:01:46 +0100

    Update to edition 2024

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use clap::Parser;
-use xtask_common::{clap, Cli, Empty};
+use xtask_common::{Cli, Empty, clap};
 
 fn main() -> Result<()> {
     let cli: Cli<Empty> = Cli::parse();
